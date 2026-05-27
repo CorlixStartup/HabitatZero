d3ac8fcc65a0d1475072242cc777d4f45289574f
SarahRSilva <ribeirosarah0912@gmail.com>
2026-05-27 20:37:05 -0300

    first commmit

---
+++ backend/src/main/java/br/com/gs/habitatzero/HabitatzeroApplication.java
@@ -0,0 +1,13 @@
+package br.com.gs.habitatzero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitatzeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitatzeroApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/gs/habitatzero/HabitatzeroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gs.habitatzero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitatzeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
