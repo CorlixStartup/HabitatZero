2c2885c350744677a8fd0473134f3f94047e0a2b
G3n4r00 <rm551986@fiap.com.br>
2026-06-05 19:00:51 -0300

    gradle update

--- front/app/build.gradle.kts
+++ front/app/build.gradle.kts
@@ -34,6 +34,7 @@ android {
 
 dependencies {
     implementation(libs.androidx.activity.ktx)
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.ktx)
